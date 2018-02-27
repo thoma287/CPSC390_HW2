d1ad9821c12f82624dec90fe88f28ee4b768a482
Shevis Johnson <johns612@mail.chapman.edu>
2018-02-27 00:25:04 -0800

    mmmmmm cheese

--- Euclidean.cpp
+++ Euclidean.cpp
@@ -8,11 +8,18 @@
 
 using namespace std;
 
+struct point {
+  int x;
+  int y;
+};
+
 class Euclidean {
   public:
     char** mapData;
-    
     int mapSize;
+
+    point initial;
+    point goal;
 
     Euclidean(vector< vector < char > > inputMap, int mapSize);
     ~Euclidean();
@@ -27,8 +34,17 @@ Euclidean::Euclidean(vector< vector < char > > inputMap, int inputSize){
     this->mapData[i] = new char [inputSize];
     for (int j = 0; j < inputSize; ++j) {
       this->mapData[i][j] = inputMap[i][j];
+      if(inputMap[i][j] == 'I'){
+        this->initial.x = i;
+        this->initial.y = j;
+      }
+      if(inputMap[i][j] == 'G'){
+        this->goal.x = i;
+        this->goal.y = j;
+      }
     }
   }
+  this->getPath();
 }
 
 Euclidean::~Euclidean() {
@@ -38,11 +54,14 @@ Euclidean::~Euclidean() {
 int* Euclidean::getPath(){
   int* hello = new int(5);
   cout << "Map loaded successfully!" << endl;
+  cout << " 0123456789" << endl;
   for (int i = 0; i < mapSize; ++i) {
+    cout << to_string(i);
     for (int j = 0; j < mapSize; ++j) {
       cout << mapData[i][j];
     }
     cout << endl;
   }
+  printf("Initial: (%i,%i)\nGoal: (%i,%i)\n", this->initial.x, this->initial.y, this->goal.x, this->goal.y);
   return hello;
 }
