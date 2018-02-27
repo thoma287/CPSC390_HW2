10ca14676d402aef0c44bf8cc4dc0c24c264e91b
Shevis Johnson <johns612@mail.chapman.edu>
2018-02-26 23:42:48 -0800

    Euclidean

--- Euclidean.cpp
+++ Euclidean.cpp
@@ -1,23 +1,48 @@
+// 1. f(N) = Euclidean Distance from N to goal
+//    length of straight line between 2 options
+//    E((i,j),(i',j')) = sqrt[(i-i')^2+(j-j')^2]
+
+
+#include <iostream>
+#include <vector>
+
 using namespace std;
-#include stdio.h
-#include <array>
 
-public class Euclidean{
-public:
-  char array<array<char,size>,size> table;
+class Euclidean {
+  public:
+    char** mapData;
+    
+    int mapSize;
 
+    Euclidean(vector< vector < char > > inputMap, int mapSize);
+    ~Euclidean();
 
-  char* Euclidean(char arr[][n]);
-
-  int* getPath(char arr[][n])();
-
-
-
+    int* getPath();
 };
 
-char* Euclidean::Euclidean(char arr[][n]){
-
+Euclidean::Euclidean(vector< vector < char > > inputMap, int inputSize){
+  this->mapSize = inputSize;
+  this->mapData = new char* [inputSize];
+  for (int i = 0; i < inputSize; ++i) {
+    this->mapData[i] = new char [inputSize];
+    for (int j = 0; j < inputSize; ++j) {
+      this->mapData[i][j] = inputMap[i][j];
+    }
+  }
 }
-int* Euclidean::getPath(Euclidean table){
 
+Euclidean::~Euclidean() {
+  delete mapData;
+}
+
+int* Euclidean::getPath(){
+  int* hello = new int(5);
+  cout << "Map loaded successfully!" << endl;
+  for (int i = 0; i < mapSize; ++i) {
+    for (int j = 0; j < mapSize; ++j) {
+      cout << mapData[i][j];
+    }
+    cout << endl;
+  }
+  return hello;
 }
--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ Cost: for every move +1
 #include <fstream>
 #include <vector>
 #include <string>
+#include "Euclidean.cpp"
 
 using namespace std;
 
@@ -56,14 +57,9 @@ int main(int argc, char *argv[]) {
               throw 1;
             }
           }
-          cout << "Map loaded successfully!" << endl;
-          for (int i = 0; i < mapSize; ++i) {
-            for (int j = 0; j < mapSize; ++j) {
-              cout << mapData[i][j];
-            }
-            cout << endl;
-          }
-          
+          // do stuff with mapData
+          Euclidean test (mapData, mapSize);
+          test.getPath();
         }
       } catch (const exception& e) {
         cerr << "Invalid file format." << endl;
